06ddde68daeed17103c3d80f06837f14e7b24262
Aerok925 <sokolov.maksim.1976@mail.ru>
2022-07-29 19:40:34 +0300

    dev02

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,7 +1,53 @@
-package main
+package dev02
 
-import "fmt"
+import (
+	"errors"
+	"strings"
+	"unicode"
+)
 
-func main() {
-	fmt.Println("Qwe")
+func CheckValidStr(str string) bool {
+	beforeNum := false
+	runeStr := []rune(str)
+	if unicode.IsNumber(runeStr[0]) {
+		return false
+	}
+	for _, i := range runeStr {
+		if unicode.IsNumber(i) {
+			if beforeNum == true {
+				return false
+			} else {
+				beforeNum = true
+			}
+		} else {
+			beforeNum = false
+		}
+	}
+	return true
+}
+
+func UnpackingStr(str string) (string, error) {
+	if len(str) == 0 {
+		return "", nil
+	}
+	if CheckValidStr(str) == false {
+		return "", errors.New("not valid string")
+	}
+	retStr := strings.Builder{}
+	runeStr := []rune(str)
+	letterСount := 1
+	run := rune(1)
+	for _, r := range runeStr {
+		if unicode.IsNumber(r) {
+			letterСount = int(r-48) - 1
+			for i := 0; i < letterСount; i++ {
+				retStr.WriteRune(run)
+			}
+		} else {
+			run = r
+			retStr.WriteRune(run)
+			letterСount = 1
+		}
+	}
+	return retStr.String(), nil
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,9 +1,66 @@
-package main
+package dev02
 
 import (
 	"testing"
 )
 
-func TestADdd(t *testing.T) {
+func TestUnpackingStr(t *testing.T) {
+	testStr := "a4bc2d5e"
+	resultStr := "aaaabccddddde"
+
+	testStr1 := "abcd"
+	resultStr1 := "abcd"
+
+	testStr2 := "45"
+	resultStr2 := ""
+
+	testStr3 := ""
+	resultStr3 := ""
+
+	t.Run("simple", func(t *testing.T) {
+		realStr, _ := UnpackingStr(testStr1)
+		if realStr != resultStr1 {
+			t.Error("real string", realStr, "!=", resultStr1)
+		}
+	})
+
+	t.Run("str with numeric", func(t *testing.T) {
+		realStr, _ := UnpackingStr(testStr)
+		if realStr != resultStr {
+			t.Error("real string", realStr, "!=", resultStr)
+		}
+		realStr, _ = UnpackingStr("a1s1d1f1g1")
+		if realStr != "asdfg" {
+			t.Error("real string", realStr, "!=", "asdfg")
+		}
+	})
+
+	t.Run("russian", func(t *testing.T) {
+		realStr, _ := UnpackingStr("фывфывфыв")
+		if realStr != "фывфывфыв" {
+			t.Error("real string", realStr, "!=", "фывфывфыв")
+		}
+	})
+
+	t.Run("russian with numeric", func(t *testing.T) {
+		realStr, _ := UnpackingStr("ф2ы1в5ф1ы7в1ф1ы3в")
+		if realStr != "ффывввввфыыыыыыывфыыыв" {
+			t.Error("real string", realStr, "!=", "ффывввввфыыыыыыывфыыыв")
+		}
+	})
+
+	t.Run("error str", func(t *testing.T) {
+		realStr, _ := UnpackingStr(testStr2)
+		if realStr != resultStr2 {
+			t.Error("real string", realStr, "!=", resultStr2)
+		}
+	})
+
+	t.Run("void str", func(t *testing.T) {
+		realStr, _ := UnpackingStr(testStr3)
+		if realStr != resultStr3 {
+			t.Error("real string", realStr, "!=", resultStr3)
+		}
+	})
 
 }
